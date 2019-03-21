9584a5aeebf1e46ae16042bd03c9dc0d932456b6
MrXyfir <mr@xyfir.com>
2019-03-20 18:58:50 -0700

    Add webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
-  entry: './src/ReactEmbedGist.js',
+  mode: process.env.NODE_ENV,
+
+  entry: './src/ReactGithubGist.tsx',
+
   output: {
-    path: __dirname,
-    filename: 'main.js',
-    libraryTarget: 'commonjs2'
+    libraryTarget: 'umd',
+    filename: 'ReactGithubGist.js',
+    library: 'ReactGithubGist',
+    path: path.resolve(__dirname, 'dist')
   },
+
   resolve: {
-    extensions: ['.js', '.jsx']
+    modules: [__dirname, 'node_modules'],
+    extensions: ['.tsx']
   },
+
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.tsx$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-        query: {
-          cacheDirectory: true,
-          presets: ['react', 'es2015']
+        options: {
+          presets: [
+            ['@babel/preset-typescript', { isTSX: true, allExtensions: true }],
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: [
+                    'last 3 Chrome versions',
+                    'last 3 Firefox versions'
+                  ]
+                }
+              }
+            ],
+            '@babel/preset-react'
+          ],
+          plugins: ['@babel/plugin-proposal-class-properties']
         }
       }
     ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ],
+
+  externals: {
+    react: 'react'
   }
 };
